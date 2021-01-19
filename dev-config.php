44a07015c1a99ec72067d9557cb30e99b5a9cad5
C-Elliott44 <C.Elliott.Dev@Gmail.com>
2021-01-19 12:15:25 -0800

    adding config files, local not working... staging?

---
+++ dev-config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * The dev-config.php DB credentials
+ *
+ */
+/** The name of the database for WordPress */
+define('DB_NAME', 'dev.infojacket.com');
+
+/** MySQL database username */
+define('DB_USER', 'infojacket-db');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'b701vGHPb0glQ4yACFFG');
+
+/** MySQL hostname */
+define('DB_HOST', 'dev.dhali.com');
+
+
